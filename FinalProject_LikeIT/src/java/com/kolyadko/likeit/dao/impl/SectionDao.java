63e707525149b1e8d58dec0fd98384783c9c6485
darya_kolyadko <skvoznak1996@gmail.com>
2016-07-30 05:42:27 +0300

    Add dao and service layers for entity 'section'

---
+++ FinalProject_LikeIT/src/java/com/kolyadko/likeit/dao/impl/SectionDao.java
@@ -0,0 +1,128 @@
+package com.kolyadko.likeit.dao.impl;
+
+import com.kolyadko.likeit.dao.AbstractDao;
+import com.kolyadko.likeit.entity.Section;
+import com.kolyadko.likeit.exception.DaoException;
+import com.kolyadko.likeit.pool.ConnectionWrapper;
+import org.apache.commons.lang3.StringUtils;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * Created by DaryaKolyadko on 29.07.2016.
+ */
+public class SectionDao extends AbstractDao<Integer, Section> {
+    private static final String ALL_COLUMNS = "section_id, major_section_id, name, question_num, answer_num, archive";
+    private static final String INSERT_COLUMNS = "major_section_id, name";
+
+    private static final String ORDER_BY_ID = " ORDER BY section_id";
+    private static final String EXISTING = "  AND archive='false'";
+
+    private static final String SELECT_ALL = "SELECT " + ALL_COLUMNS + " FROM section";
+    private static final String FIND_BY_ID = SELECT_ALL + "  WHERE section_id=?";
+    private static final String FIND_BY_MAJOR_SECTION_ID = SELECT_ALL + "  WHERE major_section_id=?";
+    private static final String FIND_MAJOR_SECTIONS = SELECT_ALL + "  WHERE major_section_id IS NULL";
+    private static final String CREATE = "INSERT INTO section (" + INSERT_COLUMNS + ") VALUES(" +
+            StringUtils.repeat("?", ", ", INSERT_COLUMNS.split(",").length) + ");";
+
+    public SectionDao(ConnectionWrapper connection) {
+        super(connection);
+    }
+
+    @Override
+    public Section findById(Integer id) throws DaoException {
+        ArrayList<Section> sections = findBy(FIND_BY_ID, id);
+
+        if (sections != null && sections.size() > 0) {
+            return sections.get(0);
+        }
+
+        return null;
+    }
+
+    @Override
+    public ArrayList<Section> findAll() throws DaoException {
+        try {
+            return findWithStatement(SELECT_ALL + ORDER_BY_ID);
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+    }
+
+    @Override
+    public void create(Section section) throws DaoException {
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(CREATE);
+            preparedStatement.setInt(1, section.getMajorSectionId());
+            preparedStatement.setString(2, section.getName());
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        } finally {
+            closeStatement(preparedStatement);
+        }
+    }
+
+    public ArrayList<Section> findByMajorId(int majorSectionId) throws DaoException {
+        return findBy(FIND_BY_MAJOR_SECTION_ID + ORDER_BY_ID, majorSectionId);
+    }
+
+    public ArrayList<Section> findExistingByMajorId(int majorSectionId) throws DaoException {
+        return findBy(FIND_BY_MAJOR_SECTION_ID + EXISTING + ORDER_BY_ID, majorSectionId);
+    }
+
+    public ArrayList<Section> findMajorSections() throws DaoException {
+        try {
+            return findWithStatement(FIND_MAJOR_SECTIONS + ORDER_BY_ID);
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+    }
+
+    public ArrayList<Section> findExistingMajorSections() throws DaoException {
+        try {
+            return findWithStatement(FIND_MAJOR_SECTIONS + EXISTING + ORDER_BY_ID);
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+    }
+
+    private ArrayList<Section> findBy(String query, int someId) throws DaoException {
+        ArrayList<Section> sections = new ArrayList<>();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet;
+
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, someId);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                sections.add(readEntity(resultSet));
+            }
+
+            return sections;
+        } catch (SQLException e) {
+            throw new DaoException(e.getMessage());
+        } finally {
+            closeStatement(preparedStatement);
+        }
+    }
+
+    @Override
+    protected Section readEntity(ResultSet resultSet) throws SQLException {
+        Section section = new Section();
+        section.setId(resultSet.getInt("section_id"));
+        section.setMajorSectionId(resultSet.getInt("major_section_id"));
+        section.setName(resultSet.getString("name"));
+        section.setQuestionNum(resultSet.getInt("question_num"));
+        section.setAnswerNum(resultSet.getInt("answer_num"));
+        section.setArchive(resultSet.getBoolean("archive"));
+        return section;
+    }
+}
---
+++ FinalProject_LikeIT/src/java/com/kolyadko/likeit/service/impl/SectionService.java
@@ -0,0 +1,89 @@
+package com.kolyadko.likeit.service.impl;
+
+import com.kolyadko.likeit.dao.impl.SectionDao;
+import com.kolyadko.likeit.entity.Section;
+import com.kolyadko.likeit.exception.DaoException;
+import com.kolyadko.likeit.exception.ServiceException;
+import com.kolyadko.likeit.pool.ConnectionWrapper;
+import com.kolyadko.likeit.service.AbstractService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+/**
+ * Created by DaryaKolyadko on 29.07.2016.
+ */
+public class SectionService extends AbstractService<Integer, Section> {
+    @Override
+    public Section findById(Integer id) throws ServiceException {
+        ConnectionWrapper connection = getConnectionWrapper();
+        SectionDao sectionDao = new SectionDao(connection);
+
+        try {
+            return sectionDao.findById(id);
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public ArrayList<Section> findAll() throws ServiceException {
+        ConnectionWrapper connection = getConnectionWrapper();
+        SectionDao sectionDao = new SectionDao(connection);
+
+        try {
+            return sectionDao.findAll();
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        } finally {
+            connection.close();
+        }
+    }
+
+    @Override
+    public void create(Section section) throws ServiceException {
+        ConnectionWrapper connection = getConnectionWrapper();
+        SectionDao sectionDao = new SectionDao(connection);
+
+        try {
+            sectionDao.create(section);
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        } finally {
+            connection.close();
+        }
+    }
+
+    public HashMap selectSectionsCatalogueTree(boolean isAdmin)
+            throws ServiceException {
+        ConnectionWrapper connection = getConnectionWrapper();
+
+        SectionDao sectionDao = new SectionDao(connection);
+        LinkedHashMap<Section, LinkedHashMap<Section, ArrayList<Section>>> catalogue = new LinkedHashMap<>();
+        ArrayList<Section> majorSections;
+
+        try {
+            majorSections = isAdmin ? sectionDao.findMajorSections() : sectionDao.findExistingMajorSections();
+
+            for (Section majorSection : majorSections) {
+                ArrayList<Section> sections = isAdmin ? sectionDao.findByMajorId(majorSection.getId()) :
+                        sectionDao.findExistingByMajorId(majorSection.getId());
+                LinkedHashMap<Section, ArrayList<Section>> sectionsWithSubsections = new LinkedHashMap<>();
+
+                for (Section section : sections) {
+                    sectionsWithSubsections.put(section, isAdmin ? sectionDao.findByMajorId(section.getId()) :
+                            sectionDao.findExistingByMajorId(section.getId()));
+                }
+
+                catalogue.put(majorSection, sectionsWithSubsections);
+            }
+
+            return catalogue;
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        } finally {
+            connection.close();
+        }
+    }
+}
