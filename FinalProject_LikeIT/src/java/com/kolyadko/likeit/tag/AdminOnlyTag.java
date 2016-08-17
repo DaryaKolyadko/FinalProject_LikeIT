35982117eb565915b97b26c5182cbfc551d9bd74
darya_kolyadko <skvoznak1996@gmail.com>
2016-08-18 02:35:23 +0300

    add own tag (AdminOnlyTag)

---
+++ FinalProject_LikeIT/src/java/com/kolyadko/likeit/tag/AdminOnlyTag.java
@@ -0,0 +1,29 @@
+package com.kolyadko.likeit.tag;
+
+import com.kolyadko.likeit.entity.User;
+import com.kolyadko.likeit.memorycontainer.impl.ObjectMemoryContainer;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+/**
+ * Created by DaryaKolyadko on 08.08.2016.
+ */
+public class AdminOnlyTag extends BodyTagSupport {
+    private static final String SESSION_ATTR_USER = "userContainer";
+
+    @Override
+    public int doStartTag() throws JspException {
+        ObjectMemoryContainer user = (ObjectMemoryContainer) pageContext.getSession().getAttribute(SESSION_ATTR_USER);
+
+        if (user != null) {
+            User currentUser = (User) user.getObject();
+
+            if (currentUser.isAdmin()) {
+                return EVAL_BODY_INCLUDE;
+            }
+        }
+
+        return SKIP_BODY;
+    }
+}
